fb1f49f23b293cd1b34fd5375c246d6ce4c75b3a
bindhushree <bindhushreeb05@gmail.com>
2024-01-20 11:16:25 +0530

    done

--- src/index.jsx
+++ src/index.jsx
@@ -8,6 +8,5 @@ import Data from './components/Datacomponents'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App data={Data}/>
-   {/* <AppClass data={Data}/> */}
   </React.StrictMode>,
 )
